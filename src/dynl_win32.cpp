b6c2d78371c4f58bc8b87771f41a255549c59a2a
doocman <rvonsonsnadtz@gmail.com>
2024-05-26 21:34:19 +0200

    fix windows error

--- src/dynl_win32.cpp
+++ src/dynl_win32.cpp
@@ -5,16 +5,16 @@
 #include <dynl/platform_details/dynl_win32.hpp>
 
 namespace dynl::_backend {
-dynamic_function_symbol do_find_function(dynamic_library_pointer *lib,
-                                         char const *name,
-                                         error_callback const &ecb) {
+dynamic_function_symbol src_find_function(dynamic_library_pointer *lib,
+                                          char const *name,
+                                          error_callback const &ecb) {
   return hh_find_function(lib, name, ecb);
 }
 
-dynamic_library_pointer *do_load_library(char const *p,
-                                         error_callback const &ecb) {
+dynamic_library_pointer *src_load_library(char const *p,
+                                          error_callback const &ecb) {
   return hh_load_library(p, ecb);
 }
 
-void do_release_library(dynamic_library_pointer *p) { hh_release_library(p); }
+void src_release_library(dynamic_library_pointer *p) { hh_release_library(p); }
 } // namespace dynl::_backend
